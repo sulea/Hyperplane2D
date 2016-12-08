1659f388feeadcec36e04fd45f2bb7f9f90b9b36
sulea <attila.addis@gmail.com>
2016-12-08 21:21:06 +0100

    linear classification of two classes finished

--- com.sulea.python.first/src/FirstNNTest.py
+++ com.sulea.python.first/src/FirstNNTest.py
@@ -3,23 +3,45 @@ Created on Oct 26, 2016
 
 @author: sulea
 '''
-inputset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# fix random seed for reproducibility
+# seed = 7
+# np.random.seed(seed)
+
+# load and transpose input data set
+inputset = np.loadtxt("learningsets/hyperplane2d_INPUTS.csv", delimiter=",")
+inputset = inputset.transpose()
+
+# load output data set
+outputset = np.loadtxt("learningsets/hyperplane2d_OUTPUTS.csv", delimiter=",")
+outputset = outputset
+
+# plot the training set
+plt.scatter(inputset[:,0], inputset[:,1], s=40, c=outputset, cmap=plt.get_cmap('Spectral'))
+plt.show()
+
+# build the simple perceptron
 from keras.models import Sequential
 model = Sequential() 
 
-from keras.layers import Dense, Activation
-# 1st layer
-# Dense: 2 inputs, 1 outputs 
-model.add(Dense(output_dim=1, input_dim=2))
-# Activation function: rectified linear unit
-model.add(Activation("relu"))
+from keras.layers import Dense
+# The neuron
+# Dense: 2 inputs, 1 outputs . Linear activation
+model.add(Dense(output_dim=1, input_dim=2, activation="linear"))
+model.compile(loss='mean_squared_error', metrics=['accuracy'], optimizer='sgd')
+model.fit(inputset, outputset, nb_epoch=100, batch_size=10)
 
-model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+# evaluate the model
+scores = model.evaluate(inputset, outputset)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-X_train = [[-6,1],[3,-5]]
+# calculate prediction for one input
+testset = np.array([[5],[1]])
+predictions = model.predict(testset.transpose())
 
-
-Y_train = [-1,1]
-
-model.fit(X_train, Y_train, nb_epoch=5, batch_size=10)
+# round predictions
+rounded = [round(x) for x in predictions]
+print(rounded)
